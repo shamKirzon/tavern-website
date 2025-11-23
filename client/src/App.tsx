4b3a706c703b3c26e6d8f2396d509a1f6e2c28d4
shammy14-1 <shammysuyat@gmail.com>
2025-11-24 00:46:04 +0800

    chore: navigation side bar setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,27 @@
-import Dashboard from "./components/Dashboard";
-import LoginPage from "./components/LoginPage";
+import Dashboard from "./pages/Dashboard";
+import LoginPage from "./pages/LoginPage";
 import { Route, Routes } from "react-router-dom";
-import OrderTracking from "./components/OrderTracking";
-import ReportAndAnalytics from "./components/ReportAndAnalytics";
-import ReservationManagement from "./components/ReservationManagement";
-import EmployeeManagement from "./components/EmployeeManagement";
+import OrderTracking from "./pages/OrderTracking";
+import ReportAndAnalytics from "./pages/ReportAndAnalytics";
+import ReservationManagement from "./pages/ReservationManagement";
+import EmployeeManagement from "./pages/EmployeeManagement";
+import Layout from "./components/app/layout";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/order-tracking" element={<OrderTracking />} />
-      <Route path="/report-and-analytics" element={<ReportAndAnalytics />} />
-      <Route
-        path="/reservation-management"
-        element={<ReservationManagement />}
-      />
-      <Route path="/employee-management" element={<EmployeeManagement />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/order-tracking" element={<OrderTracking />} />
+        <Route path="/report-and-analytics" element={<ReportAndAnalytics />} />
+        <Route
+          path="/reservation-management"
+          element={<ReservationManagement />}
+        />
+        <Route path="/employee-management" element={<EmployeeManagement />} />
+      </Routes>
+    </Layout>
   );
 };
 
